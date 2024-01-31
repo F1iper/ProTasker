ab1f0bc1b1125c8d6929283d3574742cd800d712
f1iper <ftimofiejew@gmail.com>
2024-01-31 21:23:14 +0100

    Fight with Docker on WSL2 issue on Win10 continues

--- src/test/java/com/pro/tasker/task/service/integration/TaskServiceIntegrationTest.java
+++ src/test/java/com/pro/tasker/task/service/integration/TaskServiceIntegrationTest.java
@@ -5,49 +5,78 @@ import com.pro.tasker.task.entity.Task;
 import com.pro.tasker.task.entity.TaskStatus;
 import com.pro.tasker.task.repository.TaskRepository;
 import com.pro.tasker.task.service.TaskService;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.List;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
 @SpringBootTest
 public class TaskServiceIntegrationTest {
 
-    @ClassRule
-    public static PostgreSQLContainer postgres = new PostgreSQLContainer(
-            "postgres:14.9")
+    @Container
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres")
             .withDatabaseName("protasker")
             .withUsername("postgres")
             .withPassword("admin");
 
+//    @Autowired
+//    private TaskService taskService;
 
-    @Autowired
-    private TaskService taskService;
-
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("postgres", postgres::getUsername);
+        registry.add("admin", postgres::getPassword);
+    }
     @Autowired
     private TaskRepository taskRepository;
 
-    @Test
-    public void shouldCreateAndRetrieveTask() {
-        //given
-        Task taskToCreate = new Task(null, "test task", "description", TaskStatus.BACKLOG);
 
-        //when
-        Task createdTask = taskService.createTask(taskToCreate);
-
-        //then
-        Assertions.assertAll(
-                () -> assertNotNull(createdTask.getId()),
-                () -> assertEquals(taskToCreate.getTitle(), createdTask.getTitle()),
-                () -> assertEquals(taskToCreate.getDescription(), createdTask.getDescription())
-        );
+    @BeforeEach
+    void setUp() {
+        List<Task> taskList = List.of(new Task(null, "title", "description", TaskStatus.BACKLOG));
+        taskRepository.saveAll(taskList);
     }
+
+    @Test
+    void shouldReturnTaskByTitle() {
+        Task task = taskRepository.findByTitle("title");
+        assertThat(task).isNotNull();
+    }
+
+    @Test
+    void connectionEstablished() {
+        assertThat(postgres.isCreated()).isTrue();
+        assertThat(postgres.isRunning()).isTrue();
+
+    }
+
+    //    @Test
+//    public void shouldCreateAndRetrieveTask() {
+//        //given
+//        Task taskToCreate = new Task(null, "test task", "description", TaskStatus.BACKLOG);
+//
+//        //when
+//        Task createdTask = taskService.createTask(taskToCreate);
+//
+//        //then
+//        assertAll(
+//                () -> Assertions.assertNotNull(createdTask.getId()),
+//                () -> Assertions.assertEquals(taskToCreate.getTitle(), createdTask.getTitle()),
+//                () -> Assertions.assertEquals(taskToCreate.getDescription(), createdTask.getDescription())
+//        );
+//    }
 }
