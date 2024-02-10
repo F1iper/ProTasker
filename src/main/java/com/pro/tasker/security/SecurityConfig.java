3d49c2ceac82e7cb32e4b606585584f59a669950
f1iper <ftimofiejew@gmail.com>
2024-02-10 22:29:10 +0100

    Partial JWT Implementation

---
+++ src/main/java/com/pro/tasker/security/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.pro.tasker.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+}
