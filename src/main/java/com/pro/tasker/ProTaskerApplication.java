75dc2ab61931c0fa8456c0f4fe08630382d55562
Filip T <59711082+F1iper@users.noreply.github.com>
2024-02-28 22:35:51 +0100

    Add spring cloud starter (#1)

--- src/main/java/com/pro/tasker/ProTaskerApplication.java
+++ src/main/java/com/pro/tasker/ProTaskerApplication.java
@@ -2,6 +2,7 @@ package com.pro.tasker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.core.userdetails.User;
@@ -14,6 +15,7 @@ import org.springframework.security.provisioning.UserDetailsManager;
 
 @SpringBootApplication
 @ComponentScan
+@EnableDiscoveryClient
 public class ProTaskerApplication {
 
 	public static void main(String[] args) {
